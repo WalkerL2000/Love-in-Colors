c5aa45fc4e7c61ed5996cf7429017a1b091e4a3b
WalkerL2000 <walker.lambert@cca.edu>
2021-05-05 18:27:28 -0700

    5-5-21

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('artmode');
+
+$(document).ready(function(){
+
+	$("#artmode").click(function(){
+		$('.violetartmode').toggleClass('artmode');
+	});
+
+	$("#artmode").click(function(){
+		$('.lavenderphoto').toggleClass('artmode');
+	});
+
+	$("#artmode").click(function(){
+		$('body').toggleClass('artmode');
+	});
+ 
+ 	$("#artmode").click(function(){
+		$('p').toggleClass('artmode');
+	});
+
+});
+
